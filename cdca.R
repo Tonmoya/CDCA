f3ed0f3b3e2216da095d833f27383fe85d82f9f5
Tonmoya <Tonmoya@users.noreply.github.com>
2023-05-10 11:28:53 +0530

    Update cdca.R

--- cdca.R
+++ cdca.R
@@ -1,3 +1,5 @@
+# compile this file after compiling homogeneity.R
+
 eigen_cen_community <- function(data, dataname){
 
 library(e1071)
@@ -10,23 +12,18 @@ library(stringr)
 library(ggnetwork)
 library(tidyverse)
 
-
-#data <- read.csv(file = "E:/Tonmoya/R_scripts/community/Proposed/datasets/new_normalization/corr_D1_gse53239.csv", stringsAsFactors = FALSE)
-#dim(data)
-
-#dataname = "corr_D1_gse53239"
-
-outfolder = "E:/Tonmoya/R_scripts/community/Proposed/eigen_centrality/memberships/"
+setwd("CDCA/")  # set the working directing where all the files of the program will be saved
+outfolder = "memberships/"    # specify a location where output files will be saved
 
 
 #preparing the data--------
 geneNames=data[,1]
-rawData<-data[-1] # 146 x 146
+rawData<-data[-1] 
 
 rownames(rawData) <- data[,1]
 
-corr_D1 <- as.matrix(rawData) # positive weights matrix
-corr_D1 <- abs(corr_D1)
+corr_D1 <- as.matrix(rawData) 
+corr_D1 <- abs(corr_D1)      # positive weights matrix
 #corr_D1[lower.tri(corr_D1)] <- 0 
 corr_D1[is.na(corr_D1)] <- 0
 diag(corr_D1) <- 0
@@ -41,21 +38,14 @@ min_corr <- unlist(min_corr)
 min_corr[!is.finite(min_corr)] <- 0
 max_corr_final <- max(min_corr)   # get maximum of the minimum values
 
-#max_corr_final = 0.3
+corr_D1[corr_D1<=max_corr_final] <- 0  # threshold max_corr_final
 
 
-corr_D1[corr_D1<=max_corr_final] <- 0  # threshold max_corr
-
-
-# change row and column names to numbers
-#rownames(corr_D1) <- c(1:dim(corr_D1)[1])
-#colnames(corr_D1) <- c(1:dim(corr_D1)[2])
-
-
+# Create graph from correlation matrix
 corr_graph_D <- graph_from_adjacency_matrix(corr_D1, weighted = TRUE, mode = "undirected")
 corr_graph_D <- simplify(corr_graph_D)
-#plot(corr_graph_D)
-comp_corr_graph_D <- components(corr_graph_D)  # membership, csize(cluster sizes), no(no. of clusters)
+
+comp_corr_graph_D <- components(corr_graph_D)  
 mod1_D <- modularity(corr_graph_D, membership(comp_corr_graph_D))
 
 corr_D1 <- as.data.frame(corr_D1)
@@ -63,9 +53,8 @@ corr_D1 <- as.data.frame(corr_D1)
 adj_matrix <- as_adj(corr_graph_D, type = "both")
 adj_matrix <- as.matrix(adj_matrix)
 
-#corr_D2 <- corr_D1
 
-##---------------- ALGO ---------
+##---------------- CDCA ALGO ---------
 
 
 #all_del_index <- 1:length(geneNames)
@@ -76,50 +65,35 @@ comp_new_graph <- components(new_graph)  # membership, csize(cluster sizes), no(
 #mod1_temp = mod1_D
 
 
-# STEP 1 - initialize each node with strength of the node *****************1111111111111
+# STEP 1 - initialize each node with strength of the node 
 
 deg_D <- degree(corr_graph_D) # DEGREES of all the nodes
 strength_D <- strength(corr_graph_D) # weighted degree of all nodes
 strength_D_vector <- c(strength_D)
-#connectivity <- strength_D/deg_D
 
-# get edgelist of the graph
-edge_corr <- cbind(get.edgelist(corr_graph_D), round(E(corr_graph_D)$weight, 3))   # edgelist with weights
-edge_corr <- as.data.frame(edge_corr)
 
-#edge_corr <- edge_corr[!(edge_corr$V3==0),]  # remove edges with 0 weight
+# STEP 2 - Calculate eigen vector centrality of the graph
 
-# STEP 2 - Centrality score of each node = strength + index of all neighbouring nodes -- loop here
-
-#centrality <- adj_matrix %*% strength_D_vector   ## A x VS = VC ***** USE THIS 
-for (ct in 1:floor(length(strength_D)/2)) {
-  degree_sum <- adj_matrix %*% strength_D_vector
-  centrality <- degree_sum/max(degree_sum)
-  strength_D_vector = centrality
-}
+eigen_cen <- eigen_centrality(corr_graph_D, directed = FALSE)  # EIGEN VECTOR CENTRALITY
+centrality <- eigen_cen$vector
 
 centrality_nonzero <- subset(centrality, centrality>0) # remove outliers with 0 centrality
 centrality_sort <- as.data.frame(sort(centrality_nonzero, decreasing = TRUE))
 
 all_scores <- cbind(centrality,deg_D,strength_D,visited=0)
 
-#median_cen <- sort(centrality_nonzero)[length(centrality_nonzero)/2] # MEDIAN of centrality
-#centers <- length(which(centrality>median_cen)) # number of values less than median
-
 # Top N highest values as cluster centers
-#cluster_centers <- order(centrality, decreasing=TRUE)[1:(floor(length(centrality)/5))]
 cluster_centers <- order(centrality, decreasing=TRUE)[1:5]
 
 all_scores[cluster_centers,"visited"] = 1  # all clusters centers are made visited
 
-# SUB-CLUSTER
-#cen = cluster_centers[cen_num]
-#check correlations of cluster centers with nodes that are not in cluster center list -
-# connect cluster center with node with highest correlation -
+
+# STEP 3 - Find SUB-COMMUNITY --------
+
+# check correlations of cluster centers with all other nodes -
+# connect node with cluster center with which it has highest correlation -
 # initial number of clusters will be number of cluster centers found
 # visited nodes to be mark visited - loop until no more unvisited nodes left
-#max_cen <- rep(NA, comp_new_graph$no) # empty list
-
 
 for (other_nodes in 1:length(centrality)) {
   
@@ -129,9 +103,7 @@ for (other_nodes in 1:length(centrality)) {
   
   if(all_scores[other_nodes,"visited"]==0 && all_scores[other_nodes,"deg_D"]!=0){
     all_scores[other_nodes,"visited"] = 1 
-    
-    #print(paste0(all_scores[other_nodes,1]))
-    
+   
     for (k in 1:length(cluster_centers)) {
       # get correlation between two vertices
       get_nodes[k,] = c(other_nodes, cluster_centers[k], corr_graph_D[other_nodes,cluster_centers[k]])
@@ -148,24 +120,21 @@ for (other_nodes in 1:length(centrality)) {
       #print(paste0(node1,"----",node2))
       new_graph[node1,node2] = corr_graph_D[node1,node2]  # SUB-GRAPH
       
-      #new_graph <- new_graph + edge(node1, node2)  # SUB-GRAPH
     }
     
     
   }
 }
 
-#Get modularity of graph after sub-graph phase
-comp_new_graph <- components(new_graph)  # membership, csize(cluster sizes), no(no. of clusters)
+#Get modularity of graph after initial sub-communities 
+comp_new_graph <- components(new_graph)  
 mod1_new_graph <- modularity(new_graph, membership(comp_new_graph))
 
 
 
-# MERGE SUB-CLUSTERS
-# with least difference in connectivity among the cluster center nodes
-# or check homogeneity of each sub cluster and merge clusters with least difference in homogeneity
-#max_cen_sort <- sort(max_cen[,1], decreasing = TRUE)
-# initialize to a max limit
+# STEP 4 - MERGE SUB-COMMUNITIES -------------------
+
+# initialize change in modularity to a max limit
 diff_mod = mod1_new_graph - mod1_D
 #
 org_graph = new_graph
@@ -177,22 +146,21 @@ for (m in 1:((comp_new_graph$no)-1)) {
   print(paste0("community1 -- ",m))
   merging_nodes = data.frame(matrix(ncol = 3))  # matrix to take maximum merging factor
   
-  inter_sub_correlation = data.frame(matrix(ncol = 3)) # matrix to save inter cluster correlation
+  inter_sub_correlation = data.frame(matrix(ncol = 3)) # matrix to save inter community correlation
   
   com_nodes1 <- communities(comp_new_graph)[m] #get nodes of a community
-  sub_graph1 <- induced_subgraph(corr_graph_D, unlist(com_nodes1)) # get the first individual cluster 
+  sub_graph1 <- induced_subgraph(corr_graph_D, unlist(com_nodes1)) # get the first individual community 
   
-  edge_sum1 = sum(E(sub_graph1)$weight)  #sum of correlation edges of own cluster
+  edge_sum1 = sum(E(sub_graph1)$weight)  #sum of correlation edges of own community - first
   avg_sum1 = edge_sum1/length(com_nodes1[[1]])  # average of correlation within community
-  merge_cen1 = intersect(unlist(com_nodes1),cluster_centers) # get the center node 1
   
   for (n in (m+1):(comp_new_graph$no)) {
     print(paste0("community2 -- ",n))
     sum_edge_cut = 0
     com_nodes2 <- communities(comp_new_graph)[n]  #get nodes of second community
-    sub_graph2 <- induced_subgraph(corr_graph_D, unlist(com_nodes2)) # get the second individual cluster 
+    sub_graph2 <- induced_subgraph(corr_graph_D, unlist(com_nodes2)) # get the second individual community 
     
-    edge_sum2 = sum(E(sub_graph2)$weight)  #sum of edges of own cluster - second
+    edge_sum2 = sum(E(sub_graph2)$weight)  #sum of edges of own community - second
     avg_sum1 = edge_sum1/length(com_nodes2[[1]])  # average of correlation within community - second
     #merge_cen2 = intersect(unlist(com_nodes2),cluster_centers) # get the center node 1
     
@@ -205,9 +173,11 @@ for (m in 1:((comp_new_graph$no)-1)) {
       }
     }
     
-    # MERGING FACTOR *************************
+    
+    # STEP 5 - Calculate MERGING FACTOR --------------------
+    
     merging_factor = (sum_edge_cut/(gsize(sub_graph1)+gsize(sub_graph2)))/((edge_sum1/gsize(sub_graph1)) + (edge_sum2/gsize(sub_graph2)))
-    merging_nodes = rbind(merging_nodes, c(m,n,merging_factor)) # cluster1, cluster2, value
+    merging_nodes = rbind(merging_nodes, c(m,n,merging_factor)) # community1, community2, value
     
   }
   
@@ -228,46 +198,43 @@ comp_org_graph = comp_new_graph
 
 
 
-# WITHOUT LOOP ---- CHECK MERGE ONE BY ONE 
-for (r in 1:dim(sort_merge_community)[1]) { # dim(sort_merge_community)[1]
-  #r=1
+for (r in 1:dim(sort_merge_community)[1]) { 
+  
   temp_graph = new_graph
   comp_temp_graph = comp_new_graph
   diff_mod1 = diff_mod
   mod_temp = mod1_new_graph
   
   com_nodes1 <- communities(comp_org_graph)[sort_merge_community[r,1]] #get nodes of a community from original
-  sub_graph1 <- induced_subgraph(corr_graph_D, unlist(com_nodes1)) # get the individual cluster
+  sub_graph1 <- induced_subgraph(corr_graph_D, unlist(com_nodes1)) # get the individual community 1
   
   com_nodes2 <- communities(comp_org_graph)[sort_merge_community[r,2]] #get nodes of a community from original
-  sub_graph2 <- induced_subgraph(corr_graph_D, unlist(com_nodes2)) # get the individual cluster 
+  sub_graph2 <- induced_subgraph(corr_graph_D, unlist(com_nodes2)) # get the individual community 2 
   
-  #new_graph[merge_cen1, merge_cen2] = corr_graph_D[merge_cen1, merge_cen2]  # CREATING FINAL GRAPH
   
   for (e1 in 1:length(unlist(com_nodes1))) {
     merge_node1 = unlist(com_nodes1)[e1]
     for (e2 in 1:length(unlist(com_nodes2))) {
       merge_node2 = unlist(com_nodes2)[e2]
       
-      new_graph[merge_node1, merge_node2] = corr_graph_D[merge_node1, merge_node2]  # CREATING FINAL GRAPH
+      new_graph[merge_node1, merge_node2] = corr_graph_D[merge_node1, merge_node2]    # CREATING FINAL GRAPH
     }
     
     sort_merge_community[r,"visited"]=1
   }
   
-  comp_new_graph <- components(new_graph)  # membership, csize(cluster sizes), no(no. of clusters)
+  comp_new_graph <- components(new_graph)  
   mod1_new_graph <- modularity(new_graph, membership(comp_new_graph))
-  diff_mod = mod1_new_graph - mod_temp  # check difference in  modularity of current and previous graph
+  diff_mod = mod1_new_graph - mod_temp                                   # check difference in  modularity of current and previous graph
   print(paste0("diff_mod---  ",diff_mod,"----mod1_new_graph----",mod1_new_graph))
   
   if(diff_mod < diff_mod1){
     print(paste0(diff_mod," < ",diff_mod1))
-    #new_graph[merge_node1, merge_node2] = 0 # remove the edge
     new_graph = temp_graph
     comp_new_graph = comp_temp_graph
-    sort_merge_community[r,"visited"]=0
+    sort_merge_community[r,"visited"]=0   # if difference in modularity is less, merge of community is not accepted
     
-    #comp_new_graph <- components(new_graph)  # membership, csize(cluster sizes), no(no. of clusters)
+    #comp_new_graph <- components(new_graph)  
     mod1_new_graph <- modularity(new_graph, membership(comp_new_graph))
     #diff_mod = mod1_new_graph - mod_temp # check difference in  modularity of current and previous graph
     print(paste0("diff_mod---  ",diff_mod,"----mod1_new_graph----",mod1_new_graph))
@@ -291,7 +258,7 @@ for (m in 1:length(communities(comp_new_graph))) {
   }
 }
 
-member_genes <- data.frame(matrix(ncol = 2)) # membership(comp_new_graph)
+member_genes <- data.frame(matrix(ncol = 2)) 
 for (p in 1:length(membership(comp_new_graph))) {
   member_genes[p,1] = geneNames[p]
   member_genes[p,2] = membership(comp_new_graph)[p]
@@ -300,10 +267,6 @@ for (p in 1:length(membership(comp_new_graph))) {
 
 write.csv(gene_name_list, file = paste0(outfolder,"community_list_",dataname,".csv"), row.names = FALSE)
 write.csv(member_genes, file = paste0(outfolder,"membership_",dataname,".csv"), row.names = FALSE)
-
-#plot(new_graph)  # 11 10 16 , MOD = 0.5001594
-
-# 83 13 27 22  1 MOD - 0.598360243448519, 105  13  27   1 MOD - 0.422931957944852
 
 
 homogen_result <- homogeneity_D(comp_new_graph,corr_graph_D,corr_D1)  
